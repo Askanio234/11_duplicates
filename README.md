7a8ccb63badd12d70eec1facb3677be234885947
Artem Chernyaev <askanio234@gmail.com>
2017-03-02 23:42:45 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# Anti-Duplicator
+# Поиск дубликатов файлов
 
-[TODO. There will be project description]
+Скрипт находит в папке (и всех подпапках к ней) файлы-дубликаты т.е. файлы с одинаковым именем и размером
 
-# Project Goals
+# Как запустить
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+Запуск на Linux:
+python duplicates.py filepath
+
+# Пример запуска:
+
+```#!bash
+
+$ E:\Users\user\11_duplicates>python duplicates.py E:\Users\user\11_duplicates\testcases
+ text.txt (0) bytes are duplicates (2 files):
+        E:\Users\user\11_duplicates\testcases\text.txt
+        E:\Users\user\11_duplicates\testcases\somefolder\text.txt
+ sometext2.docx (12614) bytes are duplicates (2 files):
+        E:\Users\user\11_duplicates\testcases\sometext2.docx
+        E:\Users\user\11_duplicates\testcases\somefolder\somefolder2\sometext2.docx
+
+```
+
+Запуск на Windows происходит аналогично.
+
+# Цели проекта
+
+Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,33 @@
+import os
+import collections
+import argparse
+
+
+def find_all_files_in_folder(filepath):
+    all_files = collections.defaultdict(list)
+    for root, dirs, files in os.walk(filepath):
+        for file_name in files:
+            full_file_name = os.path.join(root, file_name)
+            key = (os.path.getsize(full_file_name), file_name)
+            all_files[key].append(full_file_name)
+    return all_files
+
+
+def find_duplicates(all_files):
+    for size, file_name in sorted(all_files):
+        list_of_filepaths = all_files[(size, file_name)]
+        if len(list_of_filepaths) > 1:
+            print(" {0} ({1}) bytes are duplicates "
+                "({2} files):".format(file_name, size, len(list_of_filepaths)))
+            for file_path in list_of_filepaths:
+                print("\t{0}".format(file_path))
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath", help="Путь к папке")
+    args = parser.parse_args()
+    if os.path.exists(args.filepath):
+        find_duplicates(find_all_files_in_folder(args.filepath))
+    else:
+        print("Некорректный путь до файла")
