8e1882488f5e73f67a3d31443d3e7ca677b00fc4
Artem Chernyaev <askanio234@gmail.com>
2017-03-16 21:07:47 +0300

    Split find and print functionality to separate functions

--- duplicates.py
+++ duplicates.py
@@ -10,17 +10,27 @@ def find_all_files_in_folder(filepath):
             full_file_name = os.path.join(root, file_name)
             key = (os.path.getsize(full_file_name), file_name)
             all_files[key].append(full_file_name)
+    print(all_files)
     return all_files
 
 
 def find_duplicates(all_files):
+    duplicates = collections.defaultdict(list)
     for size, file_name in sorted(all_files):
         list_of_filepaths = all_files[(size, file_name)]
         if len(list_of_filepaths) > 1:
-            print(" {0} ({1}) bytes are duplicates "
-                "({2} files):".format(file_name, size, len(list_of_filepaths)))
-            for file_path in list_of_filepaths:
-                print("\t{0}".format(file_path))
+            key = size, file_name
+            duplicates[key].extend(list_of_filepaths)
+    return duplicates
+
+
+def print_duplicates(duplicates):
+    for size, file_name in duplicates:
+        list_of_filepaths = duplicates[(size, file_name)]
+        print("{} ({}) bytes are duplicates "
+            "{} files:".format(file_name, size, len(list_of_filepaths)))
+        for file_path in list_of_filepaths:
+            print("\t{}".format(file_path))
 
 
 if __name__ == '__main__':
@@ -28,6 +38,7 @@ if __name__ == '__main__':
     parser.add_argument("filepath", help="Путь к папке")
     args = parser.parse_args()
     if os.path.exists(args.filepath):
-        find_duplicates(find_all_files_in_folder(args.filepath))
+        duplicates = find_duplicates(find_all_files_in_folder(args.filepath))
+        print_duplicates(duplicates)
     else:
         print("Некорректный путь до файла")
